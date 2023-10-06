1ba813777a19c26ac58d3fd12fb6a045ed8d9097
JonaPierini <jonapierini@gmail.com>
2023-10-06 17:04:29 -0300

    newCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { NavBar } from "./components/NavBar/NavBar";
 import { ProductsList } from "./components/Products/ProductsList";
 import { Fotter } from "./components/Footer/Fotter";
-
 function App() {
   return (
     <>
-      <NavBar></NavBar>
+      <NavBar />
       <ProductsList />
       <Fotter />
     </>
