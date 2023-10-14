2f7a50ac4445690393ec29f6cc2544fe5319d72d
JonaPierini <jonapierini@gmail.com>
2023-10-14 18:20:05 -0300

    detail

--- src/components/ProductsDetail/ProductsDetail.jsx
+++ src/components/ProductsDetail/ProductsDetail.jsx
@@ -3,10 +3,13 @@ import { Link, useLocation } from "react-router-dom";
 
 export const ProductsDetail = () => {
   const location = useLocation();
+  console.log(location);
 
   return (
     <>
       <h1>{location.state.name}</h1>
+      <span>{location.state.description}</span>
+      <img src={location.state.image}></img>
       <Link to={"/"}>Regresar</Link>
     </>
   );
