84f0cb455cb07c49117085e33f7c893c28702f96
Martin Ballester <martinballester1@gmail.com>
2023-10-21 16:41:04 -0300

    not match

--- src/components/NoMatch/NoMatch.jsx
+++ src/components/NoMatch/NoMatch.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import notfound from "../../assets/notfound.png"
+import "./NoMatch.css";
 
 export const NoMatch = () => {
   return (
-    <div>
+    <div className="container">
       <h1>No Match</h1>
-      <Link to={"/"}>Regresar al inicio</Link>
+      <img src={notfound}></img>
+      <Link style={{ textDecoration: "none" }} to={"/"}>
+        <div className="buttonViewMoreProducts">
+          <button type="button" className="btn btn-outline-secondary">
+            Back
+          </button>
+        </div>
+      </Link>
     </div>
   );
 };
