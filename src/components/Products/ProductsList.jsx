d68e65ad3b43047dec74f3f20bf227e34f7fdf1f
JonaPierini <jonapierini@gmail.com>
2023-11-07 15:50:03 -0300

    loader

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -12,33 +12,23 @@ export const ProductsList = () => {
     setProducts(productsData);
   }, []);
 
-  const location = useLocation();
-
   return (
     <>
       <div className="products-page">
         <div className="product-list d-flex flex-wrap justify-content-around">
-          {location.pathname == "/"
-            ? products
-                .slice(0, 4)
-                .map((product) => (
-                  <ProductItem key={product.id} product={product} />
-                ))
-            : products.map((product) => (
-                <ProductItem key={product.id} product={product} />
-              ))}
+          {products.slice(0, 4).map((product) => (
+            <ProductItem key={product.id} product={product} />
+          ))}
         </div>
       </div>
-      {location.pathname == "/" && (
-        <>
-          <div style={{ marginTop: "2rem" }}>
-            <Link to={"/product"} className="button-style">
-              Explore more products
-            </Link>
-          </div>
-          <Card></Card>
-        </>
-      )}
+      <>
+        <div style={{ marginTop: "2rem" }}>
+          <Link to={"/product"} className="button-style">
+            Explore more products
+          </Link>
+        </div>
+        <Card></Card>
+      </>
     </>
   );
 };
--- src/components/ViewMoreProducts/ViewMoreProducts.jsx
+++ src/components/ViewMoreProducts/ViewMoreProducts.jsx
@@ -1,19 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { ProductsList } from "../Products/ProductsList";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import productsData from "../../data/products.json"; // Importa los datos de los productos
 import "./ViewMoreProducts.css";
+import { ProductItem } from "../Products/ProductItem";
 
 export const ViewMoreProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    setProducts(productsData);
+  }, []);
+
   return (
     <>
-      <ProductsList />
-      <Link style={{ textDecoration: "none" }} to={"/"}>
-        <div className="buttonViewMoreProducts">
-          <button type="button" className="btn btn-outline-secondary">
-            Back
-          </button>
+      <div className="products-page">
+        <h3 className="text-center">Orals Product</h3>
+        <div className="product-list d-flex flex-wrap justify-content-around">
+          {products
+            .filter((product) => product.type === "orals")
+            .map((product) => {
+              return <ProductItem key={product.id} product={product} />;
+            })}
         </div>
-      </Link>
+
+        <h3 className="text-center">Inyectable Product</h3>
+        <div className="product-list d-flex flex-wrap justify-content-around">
+          {products
+            .filter((product) => product.type === "inyectable")
+            .map((product) => {
+              return <ProductItem key={product.id} product={product} />;
+            })}
+        </div>
+      </div>
+
+      <div className="buttonViewMoreProducts">
+        <button
+          onClick={handleBack}
+          type="button"
+          className="btn btn-outline-secondary"
+        >
+          Back
+        </button>
+      </div>
     </>
   );
 };
