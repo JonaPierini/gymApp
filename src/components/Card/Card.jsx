da0c05e21df2ddbebe21b2cd6595239a4c147702
Martin Ballester <martinballester1@gmail.com>
2023-12-08 20:12:23 -0300

    cards with services

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Card.css";
 import autenticidad from "../../assets/auth.png";
 import calidad from "../../assets/healthy.png";
 import tecnologia from "../../assets/tech.png";
+import axios from "axios";
 
 export const Card = () => {
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchText = async () => {
+      try {
+        const response = await axios.get("/getCardsIndex", {
+          params: {
+            isGenoma: true,
+          },
+        });
+
+        setCards(response.data);
+      } catch (error) {
+        console.error("Error al obtener productos:", error);
+      }
+    };
+    fetchText();
+  }, []);
+
   return (
     <div className="row">
       <div className="card" style={{ width: "18rem", marginTop: "1rem" }}>
         <img src={calidad} className="card-img-top" alt="calidad" style={{ height: "240px"}} />
         <div className="card-body">
           <p className="card-text">
-            It's our mission to deliver the best quality pharmaceutical products to our customers
+            {cards.card1}
           </p>
         </div>
       </div>
@@ -19,7 +40,7 @@ export const Card = () => {
         <img src={tecnologia} className="card-img-top" alt="tecnologia" style={{ height: "240px"}}/>
         <div className="card-body">
           <p className="card-text">
-          Our production is equipped with the most advanced technology in the pharmaceutical market
+            {cards.card2}
           </p>
         </div>
       </div>
@@ -27,7 +48,7 @@ export const Card = () => {
         <img src={autenticidad} className="card-img-top" alt="autenticidad" style={{ height: "240px"}} />
         <div className="card-body">
           <p className="card-text">
-          We guarantee the offer of the latest technological authentication systems in each product to protect our consumers
+            {cards.card3}
           </p>
         </div>
       </div>
